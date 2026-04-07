628e606a1e85eac4516f6014a95b1dd738e5527d
yixind123 <yixind@amazon.com>
2026-04-07 13:48:32 -0700

    Update App.tsx
    update text

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Oh this is updated!!
       </p>
     </>
   )
